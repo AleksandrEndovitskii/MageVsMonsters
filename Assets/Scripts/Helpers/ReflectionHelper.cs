ceb40d2c132a04f6bb234eeb9734f5109dd3a2a5
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2024-05-20 18:01:14 +0200

    Add ReflectionHelper

---
+++ Assets/Scripts/Helpers/ReflectionHelper.cs
@@ -0,0 +1,10 @@
+﻿namespace MageVsMonsters.Helpers
+{
+    public class ReflectionHelper
+    {
+        public static string GetCallerMemberName([System.Runtime.CompilerServices.CallerMemberName] string callerMemberName = "")
+        {
+            return callerMemberName;
+        }
+    }
+}
