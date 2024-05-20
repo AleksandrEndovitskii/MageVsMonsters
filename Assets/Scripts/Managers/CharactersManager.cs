99259b3033f380428f58f0df77510e2598c7d7da
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2024-05-20 18:03:28 +0200

    Add CharactersManager

---
+++ Assets/Scripts/Managers/CharactersManager.cs
@@ -0,0 +1,40 @@
+﻿using Cysharp.Threading.Tasks;
+using MageVsMonsters.Models;
+using MageVsMonsters.Views;
+using MageVsMonsters.Views.Extensions;
+using UnityEngine;
+
+namespace MageVsMonsters.Managers
+{
+    public class CharactersManager : BaseManager<CharactersManager>
+    {
+        [SerializeField]
+        private PlayerView _playerViewPrefab;
+
+        public PlayerView PlayerViewInstance { get; private set; }
+
+        protected override async UniTask Initialize()
+        {
+            InstantiatePlayer();
+
+            IsInitialized = true;
+        }
+        protected override async UniTask UnInitialize()
+        {
+            IsInitialized = false;
+        }
+
+        protected override async UniTask Subscribe()
+        {
+        }
+        protected override async UniTask UnSubscribe()
+        {
+        }
+
+        private void InstantiatePlayer()
+        {
+            var playerModel = new PlayerModel();
+            PlayerViewInstance = (PlayerView)this.InstantiateElement(playerModel, _playerViewPrefab);
+        }
+    }
+}
