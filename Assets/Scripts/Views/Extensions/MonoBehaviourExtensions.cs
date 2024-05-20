0d45a88468e46fbede078b6bb6540a6e30df5f9d
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2024-05-20 18:00:49 +0200

    Add MonoBehaviourExtensions

---
+++ Assets/Scripts/Views/Extensions/MonoBehaviourExtensions.cs
@@ -0,0 +1,17 @@
+﻿using MageVsMonsters.Models;
+using UnityEngine;
+
+namespace MageVsMonsters.Views.Extensions
+{
+    public static class MonoBehaviourExtensions
+    {
+        public static BaseView<TModel> InstantiateElement<TModel>(this MonoBehaviour monoBehaviour,
+            TModel model, BaseView<TModel> prefab, Transform container = null) where TModel : IModel
+        {
+            var instance = MonoBehaviour.Instantiate(prefab, container);
+            instance.Model = model;
+
+            return instance;
+        }
+    }
+}
