dde00cdc804c9a7040c0b19eeb8365637d5e43df
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2024-05-20 19:35:07 +0200

    Add spawning of enemies

---
+++ Assets/Scripts/Managers/EnemiesManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using MageVsMonsters.Models;
+using MageVsMonsters.Views;
+using MageVsMonsters.Views.Extensions;
+using UnityEngine;
+
+namespace MageVsMonsters.Managers
+{
+    public class EnemiesManager : BaseManager<EnemiesManager>
+    {
+        [SerializeField]
+        private EnemyView _enemyViewPrefab;
+        [SerializeField]
+        private int _enemiesCount;
+
+        public List<EnemyView> EnemyViewInstances = new List<EnemyView>();
+
+        protected override async UniTask Initialize()
+        {
+            InstantiateEnemies();
+
+            IsInitialized = true;
+        }
+        protected override async UniTask UnInitialize()
+        {
+            IsInitialized = false;
+        }
+
+        protected override async UniTask Subscribe()
+        {
+        }
+        protected override async UniTask UnSubscribe()
+        {
+        }
+
+        private void InstantiateEnemies()
+        {
+            for (int i = 0; i < _enemiesCount; i++)
+            {
+                var enemyModel = new EnemyModel();
+                var enemyViewInstance = (EnemyView)this.InstantiateElement(enemyModel, _enemyViewPrefab);
+                EnemyViewInstances.Add(enemyViewInstance);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Models/EnemyModel.cs
@@ -0,0 +1,6 @@
+namespace MageVsMonsters.Models
+{
+    public class EnemyModel : CharacterModel
+    {
+    }
+}
