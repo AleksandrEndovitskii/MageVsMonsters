f96739793535f3bcfda953f351918b15270cf7c2
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2024-05-21 22:18:41 +0200

    Refactor SpawnPointComponent

--- Assets/Scripts/Components/SpawnPoints/SpawnPointComponent.cs
+++ Assets/Scripts/Components/SpawnPoints/SpawnPointComponent.cs
@@ -1,31 +1,11 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using Cysharp.Threading.Tasks;
+﻿using Cysharp.Threading.Tasks;
 using MageVsMonsters.Components.BaseComponents;
-using MageVsMonsters.Views;
-using UnityEngine;
 #pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
 
 namespace MageVsMonsters.Components.SpawnPoints
 {
     public class SpawnPointComponent : BaseComponent
     {
-#pragma warning disable 0649
-        [SerializeField]
-        private CreatureView creatureView;
-
-        [SerializeField]
-        private int spawnCount = 1;
-#pragma warning restore 0649
-
-        [NonSerialized]
-        public List<CreatureView> CreatureViewInstances = new List<CreatureView>();
-
-        private Coroutine _spawningCoroutine = null;
-
-        private float _secondsCount = 1f;
-
         protected override async UniTask Initialize()
         {
         }
@@ -38,35 +18,6 @@ namespace MageVsMonsters.Components.SpawnPoints
         }
         protected override async UniTask UnSubscribe()
         {
-        }
-
-        public void StartSpawning()
-        {
-            if (_spawningCoroutine == null) // not started yet - start it
-            {
-                _spawningCoroutine = StartCoroutine(RepeatActionEverySecondsCoroutine(
-                    _secondsCount,
-                    () =>
-                    {
-                        var creatureViewInstance = Instantiate(creatureView, this.gameObject.transform);
-                        CreatureViewInstances.Add(creatureViewInstance);
-                        if (CreatureViewInstances.Count >= spawnCount)
-                        {
-                            StopCoroutine(_spawningCoroutine);
-                            _spawningCoroutine = null;
-                        }
-                    }));
-            }
-        }
-
-        private IEnumerator RepeatActionEverySecondsCoroutine(float secondsCount, Action action)
-        {
-            while (enabled)
-            {
-                yield return new WaitForSeconds(secondsCount);
-
-                action.Invoke();
-            }
         }
     }
 }
