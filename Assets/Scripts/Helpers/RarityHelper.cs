fa0257b4577f0cf63ece72bd9b62ade0fc0469c4
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2024-05-23 03:31:10 +0200

    Refactor RarityHelper a little

--- Assets/Scripts/Helpers/RarityHelper.cs
+++ Assets/Scripts/Helpers/RarityHelper.cs
@@ -1,11 +1,22 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using MageVsMonsters.Models;
+using UnityEngine;
 
 namespace MageVsMonsters.Helpers
 {
     public static class RarityHelper
     {
+        private static Dictionary<Rarity, Color> _rarityColors = new Dictionary<Rarity, Color>
+        {
+            [Rarity.Trash] = Color.gray,
+            [Rarity.Common] = Color.white,
+            [Rarity.Uncommon] = Color.green,
+            [Rarity.Rare] = Color.blue,
+            [Rarity.Epic] = new Color(0.8f, 0.4f, 1f), // Purple
+            [Rarity.Legendary] = Color.yellow,
+            [Rarity.Mythical] = new Color(1f, 0.5f, 0.2f) // Orange
+        };
         private static Dictionary<Rarity, float> _rarityDropChances = new Dictionary<Rarity, float>
         {
             [Rarity.Trash] = 100f,
