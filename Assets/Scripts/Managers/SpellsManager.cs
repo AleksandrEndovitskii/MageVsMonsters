34c68594268f8433aaa45ff402309ac2ea995494
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2024-05-24 10:53:17 +0200

    Add SpellsManager

---
+++ Assets/Scripts/Managers/SpellsManager.cs
@@ -0,0 +1,24 @@
+using Cysharp.Threading.Tasks;
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+
+namespace MageVsMonsters.Managers
+{
+    public class SpellsManager : BaseManager<SpellsManager>
+    {
+        protected override async UniTask Initialize()
+        {
+            IsInitialized = true;
+        }
+        protected override async UniTask UnInitialize()
+        {
+            IsInitialized = false;
+        }
+
+        protected override async UniTask Subscribe()
+        {
+        }
+        protected override async UniTask UnSubscribe()
+        {
+        }
+    }
+}
