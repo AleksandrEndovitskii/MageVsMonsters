96ba6f4e546e01a54e5987abe45b9add5df9228f
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2024-05-24 12:46:18 +0200

    Add SpellDefinitionJsonObject

---
+++ Assets/Scripts/JsonObjects/SpellDefinitionJsonObject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MageVsMonsters.JsonObjects
+{
+    [Serializable]
+    public class SpellDefinitionJsonObject
+    {
+        public string Name { get; set; }
+        public int Damage { get; set; }
+    }
+}
