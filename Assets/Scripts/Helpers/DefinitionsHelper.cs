f09a5d4e35fa1421cdee22b484530fbe0134c02a
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2024-08-28 13:13:57 +0200

    Add DefinitionsHelper.GetDefinition

--- Assets/Scripts/Helpers/DefinitionsHelper.cs
+++ Assets/Scripts/Helpers/DefinitionsHelper.cs
@@ -5,6 +5,13 @@ namespace MageVsMonsters.Helpers
 {
     public static class DefinitionsHelper
     {
+        public static T GetDefinition<T>(string definitionPath)
+        {
+            var definitionJson = ResourcesHelper.LoadFromJsonInResources<string>(definitionPath);
+            var definitionJsonObject = JsonConvert.DeserializeObject<T>(definitionJson);
+
+            return definitionJsonObject;
+        }
         public static List<T> GetDefinitions<T>(string definitionsFilePath)
         {
             var definitionsJson = ResourcesHelper.LoadFromJsonInResources<string>(definitionsFilePath);
